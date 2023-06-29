89eb4fa910849569ccd941837ce0dcffee2bae76
RaeSy <rachel.sykes.2020@live.rhul.ac.uk>
2023-06-29 12:50:45 +0100

    Improve clarity of graph

--- diamonds.R
+++ diamonds.R
@@ -1,11 +1,12 @@
 
-
-
 mydata <-read.csv(file.choose())
 
 
-ggplot(data=mydata, 
+ggplot(data=mydata[mydata$carat<2.5], 
        aes(x=carat, y=price, colour=clarity)) +
-  geom_point()
+  geom_point(alpha=0.1) +
+  geom_smooth()
+
+
 
 
