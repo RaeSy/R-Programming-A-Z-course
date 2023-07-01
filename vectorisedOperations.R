3513ebcf570fc1c2aef872ecae69e84c10abb744
RaeSy <rachel.sykes.2020@live.rhul.ac.uk>
2023-07-01 13:14:26 +0100

    Add arithmetic expressions

---
+++ vectorisedOperations.R
@@ -0,0 +1,20 @@
+
+#addition
+x <- c(50, 34, 111, 7, 24,631, 20, 4, 7, 21)
+y <- c(100, 2, 56, 12, 0, 65, 93, 10, 244, 1)
+z <- x + y
+z
+
+#subtraction
+z <- x - y
+z
+
+#division
+z <- x / y
+z
+
+# multiplication
+z <- x * y
+z
+
+
