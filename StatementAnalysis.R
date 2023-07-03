a4d9007bf06c41d0e6cf8a74f8eb901506ee55a8
RaeSy <rachel.sykes.2020@live.rhul.ac.uk>
2023-07-03 16:57:26 +0100

    Add requirements as comments

---
+++ StatementAnalysis.R
@@ -0,0 +1,19 @@
+
+
+
+#requirements
+
+#monthly profit
+#monthly profit after tax 30%
+#monthly profit margin -profit after tax/revenue
+#good months - profit > mean
+#bad months - profit < mean
+#best month
+#worst month
+
+#all results as vectors
+#results as $1000 units
+#profit margin as % 
+#tax can be negative
+
+
