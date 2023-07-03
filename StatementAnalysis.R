396dd01665ada1204b1f5dfae088c2f652cb80b9
RaeSy <rachel.sykes.2020@live.rhul.ac.uk>
2023-07-03 17:24:24 +0100

    Calculate profit margin and mean profit

--- StatementAnalysis.R
+++ StatementAnalysis.R
@@ -4,12 +4,30 @@ revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 97
 expenses <- c(12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
 
 #solution
+
+#profit
 profit <- revenue - expenses
 profit
+
+#profit after tax
 tax <- 0.3
 profitPostTax <- profit - (profit*0.3)
 profitPostTax
 
+#[profit margin
+margin <- profitPostTax / revenue 
+marginPercent <- margin * 100
+marginPercent
+
+#mean
+meanCalc <- 0
+for(i in length(profitPostTax)){
+  meanCalc <- meanCalc + profitPostTax[i]
+  meanCalc
+}
+meanCalc
+
+#good months
 
 
 #requirements
